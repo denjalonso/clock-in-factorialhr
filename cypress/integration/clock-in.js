4d685c4edc7b2cb7eed8b772b0043f50b9e86198
Denis Juan Alonso Glez <denjalonso@gmail.com>
2021-02-16 12:10:49 +0000

    Should clock in

--- cypress/integration/clock-in.js
+++ cypress/integration/clock-in.js
@@ -18,4 +18,51 @@ describe("clock-in", () => {
       }
     });
   });
+  const days = [...Array(new Date().getDate()).keys()].map((day) => day + 1);
+  const months = [
+    "ene",
+    "feb",
+    "mar",
+    "abr",
+    "may",
+    "jun",
+    "jul",
+    "ago",
+    "sep",
+    "oct",
+    "nov",
+    "dic",
+  ];
+  const currentMonth = months[new Date().getMonth()];
+  for (const day in days) {
+    it(`should clock in ${days[day]} ${currentMonth}`, () => {
+      cy.visit("https://app.factorialhr.com/attendance/clock-in");
+      cy.findByText(`${days[day]} ${currentMonth}.`)
+          .parents("tr")
+          .should(
+              "have.css",
+              "background",
+              "rgba(0, 0, 0, 0) none no-repeat scroll 0% 0% / auto padding-box border-box",
+              { message: "Is weekend" }
+          )
+          .within((container) => {
+            cy.findAllByPlaceholderText(/--:--/i)
+                .first()
+                .should("not.have.value", "07:00")
+                .type("0700", { force: true });
+            cy.findAllByPlaceholderText(/--:--/i)
+                .last()
+                .type("1400", { force: true });
+            cy.findAllByRole("button").first().click({ force: true });
+            cy.findAllByRole("button").last().click({ force: true });
+            cy.findAllByPlaceholderText(/--:--/i)
+                .eq(2)
+                .type("1500", { force: true });
+            cy.findAllByPlaceholderText(/--:--/i)
+                .eq(3)
+                .type("1600", { force: true });
+            cy.findAllByRole("button").eq(1).click({ force: true });
+          });
+    });
+  }
 });
